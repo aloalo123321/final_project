622e08c2515fae66e1aef9f66986fcbce748c12a
aloalo123321 <Sabaqatamadze0@gmail.com>
2024-06-14 22:09:50 +0400

    update

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 const countdownDate = new Date("2024-07-06T00:00:00").getTime();
 
-// Update the countdown every second
 const countdownTimer = setInterval(function () {
   const now = new Date().getTime();
-  const darchenilidro = countdownDate - now;
+  const timeRemaining = countdownDate - now;
 
-  const days = Math.floor(darchenilidro / (1000 * 60 * 60 * 24));
+  const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
   const hours = Math.floor(
-    (darchenilidro % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
   );
-  const minutes = Math.floor((darchenilidro % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((darchenilidro % (1000 * 60)) / 1000);
+  const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
   document.getElementById("daysNumber").innerText = days;
   document.getElementById("hoursNumber").innerText = hours;
   document.getElementById("minutesNumber").innerText = minutes;
   document.getElementById("secondsNumber").innerText = seconds;
-  if (darchenilidro < 0) {
+
+  if (timeRemaining < 0) {
     clearInterval(countdownTimer);
     document.getElementById("daysNumber").innerText = "დაიწყო";
     document.getElementById("hoursNumber").innerText = "გაზაფხულის";
